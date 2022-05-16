5d26572ba88ce19910a509f76dc07396549393d0
Sanjeevan Elangovan <sanjeevan.elangovan@westpac.com.au>
2022-05-16 19:09:05 +0530

    Components Part 2 - completion

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
-
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,8 +22,8 @@ class App extends Component {
             <NavbarBrand href="/">Ristoreante con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
-      </div>
+        <Menu dishes={this.state.dishes} />
+       </div>
     );
   }
 }
